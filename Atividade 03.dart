a5be8870238ef364d3d35de8ba25dfa7035bcf58
adrianocefet <50247552+adrianocefet@users.noreply.github.com>
2023-04-08 16:35:00 -0300

    Add files via upload

---
+++ Atividade 03.dart
@@ -0,0 +1,25 @@
+void main() {
+  //q1
+  print(5 + 3 * 2);
+  //q2
+  print(10 / 2 - 3);
+  //q3
+  print(7 % 3);
+  //q4
+  int x = 0;
+  print(x += 5);
+  //q5
+  int y = 0;
+  print(y *= 3);
+  //q6
+  print(!(2 < 5) || (3 > 1));
+  //q7
+  int? z;
+  print(z ?? 10);
+  //q8
+  print(2 + 2 == 4 && 3 + 3 == 6);
+  //q9
+  print(5 < 3 || 4 > 2 && 6 != 6);
+  //q10
+  print(z ??= 10);
+}
